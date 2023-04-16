8f91b6a4c7ea7101070f33197a98a3edb28c97ca
damilhome <130613128+damilhome@users.noreply.github.com>
2023-04-16 20:44:06 -0300

    Sumarry

--- dino_runner/components/obstacles/large_cactus.py
+++ dino_runner/components/obstacles/large_cactus.py
@@ -2,8 +2,8 @@ import random
 
 from dino_runner.components.obstacles.obstacles import Obstacle
 
-class Cactus(Obstacle):
+class largeCactus(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        self.rect.y = 300
--- dino_runner/components/obstacles/obstacles_manager.py
+++ dino_runner/components/obstacles/obstacles_manager.py
@@ -1,18 +1,21 @@
 import pygame
+import random
 
-from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.small_cactus import Cactus
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
+        self.number = random.randrange(0, 2)
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(Cactus(LARGE_CACTUS))
 
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed, self.obstacles)
+            obstacle.update(game.game_speed, self.obstacles[self.number])
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
