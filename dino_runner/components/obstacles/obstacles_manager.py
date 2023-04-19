c4dcdba985e08b3764cf58740bbdd0d84981e437
damilhome <130613128+damilhome@users.noreply.github.com>
2023-04-19 19:57:45 -0300

    teste

--- dino_runner/components/obstacles/obstacles_manager.py
+++ dino_runner/components/obstacles/obstacles_manager.py
@@ -3,25 +3,29 @@ import random
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
+from dino_runner.components.game import Game
 from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 from dino_runner.utils.constants import DEFAULT_TYPE
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
+        self.o_game = Game()
+        self.o_score = self.o_game.score.score
 
     def update(self, game):
-        list_index = random.randint(0, 2)
-        positionY = 0
-        if list_index == 0:
-            positionY = 325
-        else:
-            positionY = 300
-        obstacle_list = [SMALL_CACTUS, LARGE_CACTUS]
+        obstacle_list = random.choice([SMALL_CACTUS, LARGE_CACTUS, "BIRD"])
+        positionY = 325 if obstacle_list == SMALL_CACTUS else 300
 
-        if len(self.obstacles) == 0:
-            if list_index == 0 or list_index == 1:
-                self.obstacles.append(Cactus(obstacle_list[list_index], positionY))
+        if len(self.obstacles) == 0 and self.o_score < 200:
+            self.obstacles.append(Cactus(SMALL_CACTUS, 325))
+        elif len(self.obstacles) == 0 and self.o_score < 400:
+            i = random.choice((SMALL_CACTUS, LARGE_CACTUS))
+            y = 325 if i == SMALL_CACTUS else 300
+            self.obstacles.append(Cactus(i, y))
+        elif len(self.obstacles) == 0 and self.o_score > 400:
+            if obstacle_list == SMALL_CACTUS or LARGE_CACTUS:
+                self.obstacles.append(Cactus(obstacle_list, positionY))
             else:
                 self.obstacles.append(Bird())
 
